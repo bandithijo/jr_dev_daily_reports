1e44f28a6ad86ae2ea228664adebf98c4f71352f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 23:06:19 +0800

    Hashes

--- giraffe.rb
+++ giraffe.rb
@@ -1,33 +1,13 @@
-friends = ["Vederis", "Hafiz", "Tendy"]
-puts friends
-puts "-" * 10
-puts friends[0] #forward
-puts friends[-2] #backward
+kode_bandara = {
+  "Surabaya" => "SUB",
+  :Bandung => "BDO",
+  :Semarang => "SRG",
+  Jakarta: "CGK",
+  Yogyakarta: "JOG",
+  90 => "HLP",
+  :91 => "SOC"
+}
 
-puts "-" * 10
-puts friends[0, 2] #start at 0 up to but not include 2
-
-puts "-" * 10
-friends = ["Vederis", "Hafiz", "Tendy"]
-friends[0] = "Leunardus"
-puts friends
-
-puts "-" * 10
-friends = Array.new
-friends[0] = "Budi"
-friends[5] = "Doremi"
-puts friends
-
-puts "-" * 10
-friends = ["Vederis", "Hafiz", "Tendy", "Rizqi"]
-puts friends.length()
-puts friends.include? "Vederis"
-puts friends.include? "vederis"
-puts "-" * 10
-puts friends.reverse()
-puts "-" * 10
-puts friends.sort()
-
-puts "-" * 10
-friends = ["Vederis", 1, "Hafiz", "Tendy", "Rizqi"]
-puts friends.reverse()
+puts kode_bandara
+puts kode_bandara[:Bandung]
+puts kode_bandara[:Jakarta]
