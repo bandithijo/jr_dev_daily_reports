ed56602853063d742a1eb8dd4f6f59493ff55c23
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 22:22:40 +0800

    Building a Calculator

--- giraffe.rb
+++ giraffe.rb
@@ -1,5 +1,15 @@
-print "Enter your name: "
-name = gets.chomp()
-print "Enter your age: "
-age = gets.chomp()
-puts ("Hello, " + name + ", you are " + age + " years old.")
+# print "Enter a number: "
+# num1 = gets.chomp()
+# print "Enter another number: "
+# num2 = gets.chomp()
+# puts "-" * 10
+# puts (num1 + num2)
+# puts (num1.to_i + num2.to_i)
+# puts (num1.to_f + num2.to_f)
+
+print "Enter a number: "
+num1 = gets.chomp().to_f
+print "Enter another number: "
+num2 = gets.chomp().to_f
+puts "-" * 10
+puts (num1 + num2)
