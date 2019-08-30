9a018c1b9ce7c2f69fa584da3c1550cb7e172fd6
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 21:22:29 +0800

    Variables

---
+++ giraffe.rb
@@ -0,0 +1,7 @@
+character_name = "Mike"
+character_age = "70"
+puts ("There once was a man named " + character_name)
+puts "he was " + character_age +" years old."
+character_name = "Tom"
+puts ("He really liked the name " + character_name)
+puts "but didn't like being " + character_age + "."
