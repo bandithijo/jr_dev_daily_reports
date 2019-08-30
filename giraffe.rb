d726d2ac207c80bf9c3582b38e74c26657a6e634
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 22:14:09 +0800

    Getting User Input

--- giraffe.rb
+++ giraffe.rb
@@ -1,33 +1,5 @@
-puts 5
-puts 5.86543
-puts -5.86543
-
-puts 5 + 9
-puts 5 - 9
-puts 5 * 9
-puts 5 / 9
-
-puts 2^3
-puts 2**3
-
-puts 10 % 3
-
-num = 20
-puts ("My fav number " + num.to_s)
-num = -20
-puts num.abs()
-num = 20.487
-puts num.round()
-num = 20.1
-puts num.ceil()
-num = 20.8
-puts num.floor()
-
-puts Math.sqrt(36)
-puts Math.log(30)
-
-puts 1 + 8 #has integer value
-puts 1.0 + 8 #has float value
-
-puts 10 / 7
-puts 10 / 7.0 #full number
+print "Enter your name: "
+name = gets.chomp()
+print "Enter your age: "
+age = gets.chomp()
+puts ("Hello, " + name + ", you are " + age + " years old.")
