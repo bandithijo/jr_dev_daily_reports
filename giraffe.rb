aec45e59d6186a7df855c146bea06e7f2c33e830
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 22:30:57 +0800

    Building a Mad Libs Game

--- giraffe.rb
+++ giraffe.rb
@@ -1,15 +1,11 @@
-# print "Enter a number: "
-# num1 = gets.chomp()
-# print "Enter another number: "
-# num2 = gets.chomp()
-# puts "-" * 10
-# puts (num1 + num2)
-# puts (num1.to_i + num2.to_i)
-# puts (num1.to_f + num2.to_f)
+print "Enter a color: "
+color = gets.chomp()
+print "Enter a plural noun: "
+plural_noun = gets.chomp()
+print "Enter a celebrity: "
+celebrity = gets.chomp()
 
-print "Enter a number: "
-num1 = gets.chomp().to_f
-print "Enter another number: "
-num2 = gets.chomp().to_f
 puts "-" * 10
-puts (num1 + num2)
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
