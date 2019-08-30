ae6654831d2a09c6fffa1240292a6156e8b07336
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 23:30:12 +0800

    Return Statement

--- giraffe.rb
+++ giraffe.rb
@@ -1,26 +1,14 @@
-# define the method block
-#def say_hi
-#  puts "Hello, Virtual Spirit!"
-#end
-
-# call the method say_hi
-#say_hi
-
-
-# define the method block
-#def say_hi(name, age)
-#  puts ("Hello, " + name + ", you are " + age.to_s + " years old")
-#end
-
-# call the method say_hi
-#say_hi("Budi", 73)
-
-
-# define the method block with default value
-def say_hi(name = "no name", age = 0)
-  puts ("Hello, " + name + ", you are " + age.to_s + " years old")
+def cube(num)
+  return num * num * num
+  5 # <= will be ignored
 end
 
-# call the method say_hi
-say_hi("Budi", 73)
-say_hi()
+puts cube(3)
+
+puts '-' * 10
+
+def cube(num)
+  return num * num * num, 70 # <= array
+end
+
+puts cube(2)[1] # <= puts array index 1
