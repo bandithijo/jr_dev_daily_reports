de96e2f2948837a67f70afca519eb4c559cf65e3
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 21:26:57 +0800

    Data Types

--- giraffe.rb
+++ giraffe.rb
@@ -1,7 +1,7 @@
-character_name = "Mike"
-character_age = "70"
-puts ("There once was a man named " + character_name)
-puts "he was " + character_age +" years old."
-character_name = "Tom"
-puts ("He really liked the name " + character_name)
-puts "but didn't like being " + character_age + "."
+name = "Rizqi Nur Assyaufi"
+occupation "Software Engineer"
+age = 120
+height = 120.52
+is_admin = false
+is_user = true
+has_house = nil
