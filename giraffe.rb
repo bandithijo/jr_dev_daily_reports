09bc01c2a09f7c43a783bde71dc741e30e7518a9
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 22:01:25 +0800

    Math & Numbers

--- giraffe.rb
+++ giraffe.rb
@@ -1,13 +1,33 @@
-phrase = "Virtual Spirit"
-puts phrase.upcase()
-puts phrase.downcase()
-phrase = "   Virtual Spirit (trail)   "
-puts phrase.strip()
-phrase = "Virtual Spirit"
-puts phrase.length()
-puts phrase.include? "Spirit"
-puts phrase.include? "spirit"
-puts phrase[0]
-puts phrase[1,6]
-puts phrase.index("V")
-puts phrase.index("Spi")
+puts 5
+puts 5.86543
+puts -5.86543
+
+puts 5 + 9
+puts 5 - 9
+puts 5 * 9
+puts 5 / 9
+
+puts 2^3
+puts 2**3
+
+puts 10 % 3
+
+num = 20
+puts ("My fav number " + num.to_s)
+num = -20
+puts num.abs()
+num = 20.487
+puts num.round()
+num = 20.1
+puts num.ceil()
+num = 20.8
+puts num.floor()
+
+puts Math.sqrt(36)
+puts Math.log(30)
+
+puts 1 + 8 #has integer value
+puts 1.0 + 8 #has float value
+
+puts 10 / 7
+puts 10 / 7.0 #full number
