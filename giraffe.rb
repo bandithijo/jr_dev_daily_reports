a288c89e86525db89877841c8eae1acabb107f03
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-08-30 21:42:47 +0800

    Working with Strings

--- giraffe.rb
+++ giraffe.rb
@@ -1,7 +1,13 @@
-name = "Rizqi Nur Assyaufi"
-occupation "Software Engineer"
-age = 120
-height = 120.52
-is_admin = false
-is_user = true
-has_house = nil
+phrase = "Virtual Spirit"
+puts phrase.upcase()
+puts phrase.downcase()
+phrase = "   Virtual Spirit (trail)   "
+puts phrase.strip()
+phrase = "Virtual Spirit"
+puts phrase.length()
+puts phrase.include? "Spirit"
+puts phrase.include? "spirit"
+puts phrase[0]
+puts phrase[1,6]
+puts phrase.index("V")
+puts phrase.index("Spi")
