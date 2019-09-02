8a09615efbb62f2c93eb9ce25019fd40dae85767
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-09-03 06:21:18 +0800

    Classes and Objects

---
+++ Step 1 Learn Ruby/25 classes-and-objects.rb
@@ -0,0 +1,19 @@
+class Book
+  attr_accessor :title, :author, :pages
+end
+
+book1 = Book.new()
+book1.title = "Ruby is awesome language!"
+book1.author = "Rizqi"
+book1.pages = 850
+
+puts book1.title
+puts book1.author
+puts book1.pages
+
+book2 = book.new()
+book2.title = "Rails is awesome framework!"
+book2.author = "Vederis"
+book2.pages = 1460
+
+puts book2.pages
