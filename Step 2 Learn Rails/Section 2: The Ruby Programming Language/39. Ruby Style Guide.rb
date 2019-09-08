8be27808a4cc7c66ef4599c7e52f90bb695c3896
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-09-09 00:43:23 +0800

    39. Ruby Style Guide

---
+++ Step 2 Learn Rails/Section 2: The Ruby Programming Language/39. Ruby Style Guide.rb
@@ -0,0 +1,19 @@
+# Method Name & Variable Assignment
+
+this_is_snake_case
+
+also_snake_case
+
+def my_name_is
+  #  and my code is here
+end
+
+
+# Class Name
+
+CamelCase
+
+camelCase    # <- not correct camel case in Ruby
+
+class SomeClass
+end
